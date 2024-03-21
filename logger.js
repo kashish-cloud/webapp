56e9b07aad755dcc85fac1960c6e84ab2e4200bd
Kashish Desai <113315022+kashishdesai03@users.noreply.github.com>
2024-03-21 03:24:56 -0400

    added winston for logging

--- logger.js
+++ logger.js
@@ -1,16 +1,37 @@
 const { createLogger, format, transports } = require("winston");
-//const { LoggingWinston } = require("@google-cloud/logging-winston");
 const path = require("path");
 
 const logFilePath = path.join("/var", "log", "webapp", "application.log");
 
-//const loggingWinston = new LoggingWinston();
-
 const logger = createLogger({
   format: format.combine(format.timestamp(), format.json()),
-  transports: [
-    new transports.File({ filename: logFilePath }) /*, loggingWinston*/,
-  ],
+  transports: [new transports.File({ filename: logFilePath })],
 });
+
+logger.add(
+  new transports.Console({
+    format: format.combine(
+      format.colorize(),
+      format.printf((info) => {
+        const { timestamp, level, message, ...meta } = info;
+        const log = {
+          severity: level === "DEFAULT" ? "INFO" : level,
+          message,
+          timestamp,
+          ...meta,
+        };
+        return JSON.stringify(log);
+      })
+    ),
+  })
+);
+
+logger.add(
+  new transports.Console({
+    format: format.printf(({ level, message, timestamp }) => {
+      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+    }),
+  })
+);
 
 module.exports = logger;
