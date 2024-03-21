270a47c2d98b0f0e1cea471b31c97a041c9af4f0
Kashish Desai <113315022+kashishdesai03@users.noreply.github.com>
2024-03-21 04:03:26 -0400

    added winston for logging

--- logger.js
+++ logger.js
@@ -5,33 +5,24 @@ const logFilePath = path.join("/var", "log", "webapp", "application.log");
 
 const logger = createLogger({
   format: format.combine(format.timestamp(), format.json()),
-  transports: [new transports.File({ filename: logFilePath })],
-});
-
-logger.add(
-  new transports.Console({
-    format: format.combine(
-      format.colorize(),
-      format.printf((info) => {
-        const { timestamp, level, message, ...meta } = info;
-        const log = {
-          severity: level === "DEFAULT" ? "INFO" : level,
-          message,
-          timestamp,
-          ...meta,
-        };
-        return JSON.stringify(log);
-      })
-    ),
-  })
-);
-
-logger.add(
-  new transports.Console({
-    format: format.printf(({ level, message, timestamp }) => {
-      return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+  transports: [
+    new transports.File({ filename: logFilePath }),
+    new transports.Console({
+      format: format.combine(
+        format.colorize(),
+        format.printf((info) => {
+          const { timestamp, level, message, ...meta } = info;
+          const log = {
+            severity: level === "DEFAULT" ? "INFO" : level,
+            message,
+            timestamp,
+            ...meta,
+          };
+          return `${timestamp} [${log.severity.toUpperCase()}]: ${log.message}`;
+        })
+      ),
     }),
-  })
-);
+  ],
+});
 
 module.exports = logger;
